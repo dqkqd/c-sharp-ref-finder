51880addd760b8c7d44f30d72dda49337c4edd6a
Khanh Duong Quoc <dqkqdlot@gmail.com>
2024-07-21 07:54:45 +0900

    refactor: using class to create function

--- parser.py
+++ parser.py
@@ -1,8 +1,9 @@
 import dataclasses
 import itertools
+from typing import Generator
 
 import tree_sitter_c_sharp as cspython
-from tree_sitter import Language, Node, Parser, Tree
+from tree_sitter import Language, Node, Parser
 
 CS_LANGUAGE = Language(cspython.language())
 parser = Parser(CS_LANGUAGE)
@@ -45,48 +46,53 @@ class Identifier:
         )
 
 
-def get_all_identifiers_from_parameters(parameters_node: Node) -> list[Identifier]:
-    identifiers: list[Identifier] = []
-    for parameter in parameters_node.children:
-        name_nodes = parameter.children_by_field_name("name")
-        identifiers.extend(map(Identifier.from_node, name_nodes))
-    return identifiers
+@dataclasses.dataclass
+class Function:
+    node: Node
+
+    @property
+    def identifier(self) -> Identifier:
+        name_node = self.node.child_by_field_name("name")
+        if name_node is None:
+            raise ValueError(f"Invalid function: {self}")
+        return Identifier.from_node(name_node)
+
+    @property
+    def parameter_identifiers(self) -> list[Identifier]:
+        def identifier_from_parameters(parameters_node: Node) -> list[Identifier]:
+            identifiers: list[Identifier] = []
+            for parameter in parameters_node.children:
+                identifiers.extend(
+                    map(Identifier.from_node, parameter.children_by_field_name("name"))
+                )
+            return identifiers
+
+        all_param_names = map(
+            identifier_from_parameters,
+            self.node.children_by_field_name("parameters"),
+        )
+        parameter_names = list(itertools.chain(*all_param_names))
+        return parameter_names
+
+    @classmethod
+    def functions_from_node(cls, node: Node) -> list["Function"]:
+        function_nodes = get_all_descendant_with_node_type(node, 'method_declaration')
+        functions = [Function(node=n) for n in function_nodes]
+        return functions
 
 
-def get_all_parameter_identifiers_from_function(function: Node) -> list[Identifier]:
-    all_param_names = map(
-        get_all_identifiers_from_parameters,
-        function.children_by_field_name("parameters"),
-    )
-    parameter_names = list(itertools.chain(*all_param_names))
-    return parameter_names
-
-
-def get_all_variables_from_function(function: Node) -> list[Identifier]:
-    return []
-
-
-def get_function_identifier(function: Node) -> Identifier:
-    name_node = function.child_by_field_name("name")
-    assert name_node is not None
-    return Identifier.from_node(name_node)
-
-
-def get_function_with_ref_from_node(node: Node):
-    for child in node.children:
-        if child.type == "method_declaration":
-            print("--------------------")
-            print(get_function_identifier(child))
-            print(get_all_parameter_identifiers_from_function(child))
-            print(get_all_variables_from_function(child))
-        get_function_with_ref_from_node(child)
-
-
-def get_function_with_ref(source: str) -> Tree:
+def get_root_node_from_source(source: str) -> Node:
     tree = parser.parse(bytes(source, "utf-8"))
-    root_node = tree.root_node
-    get_function_with_ref_from_node(root_node)
-    return tree
+    return tree.root_node
 
 
-get_function_with_ref(source_code)
+def get_all_descendant_with_node_type(node: Node, node_type: str) -> Generator[Node, None, None]:
+    if node.type == node_type:
+        yield node
+    for child in node.children:
+        yield from get_all_descendant_with_node_type(child, node_type)
+
+
+if __name__ == "__main__":
+    node = get_root_node_from_source(source_code)
+    functions = Function.functions_from_node(node)
