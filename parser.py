906a3b102d2d918b6b39b370205f46e0d80decbc
Khanh Duong Quoc <dqkqdlot@gmail.com>
2024-07-21 09:03:58 +0900

    feat: reporting

--- parser.py
+++ parser.py
@@ -1,6 +1,6 @@
 import dataclasses
 import itertools
-from typing import ClassVar, Generator
+from typing import Any, ClassVar, Generator
 
 import tree_sitter_c_sharp as cspython
 from tree_sitter import Language, Node, Parser
@@ -45,6 +45,9 @@ class Identifier:
             column=node.end_point.column,
         )
 
+    def __str__(self) -> str:
+        return f"{self.name}, line {self.row}, column {self.column}"
+
 
 @dataclasses.dataclass
 class FunctionBody:
@@ -52,26 +55,26 @@ class FunctionBody:
     MODIFIER_FIELD_TYPE: ClassVar[list[str]] = ["expression_statement"]
 
     @property
-    def maybe_modified_identifiers(self) -> list[Identifier]:
+    def maybe_modified_vars(self) -> list[Identifier]:
         maybe_modified_nodes = itertools.chain(
             get_all_descendant_with_node_type(self.node, "assignment_expression"),
             get_all_descendant_with_node_type(self.node, "declaration_expression"),
         )
         identifiers: list[Identifier] = []
         for node in maybe_modified_nodes:
-            left = node.child_by_field_name('left')
+            left = node.child_by_field_name("left")
             if left is None:
                 raise ValueError(f"Left hand side doesn't found for {left}")
             identifiers.append(Identifier.from_node(left))
-        
+
         # identifier from local variable declaration
         for node in get_all_descendant_with_node_type(self.node, "variable_declarator"):
-            ident = node.child_by_field_name('name')
+            ident = node.child_by_field_name("name")
             assert ident is not None
             identifiers.append(Identifier.from_node(ident))
-        
+
         # get identifier from function arguments
-        for node in get_all_descendant_with_node_type(self.node, 'argument'):
+        for node in get_all_descendant_with_node_type(self.node, "argument"):
             identifiers.append(Identifier.from_node(node))
 
         return identifiers
@@ -112,13 +115,26 @@ class Function:
             raise ValueError(f"Function body does not exist {self}")
         return FunctionBody(node=body)
 
-    def find_all_ref(self):
-        modified_identifiers = self.function_body.maybe_modified_identifiers
-        modified_names = set(ident.name for ident in modified_identifiers)
-        for param in self.parameter_identifiers:
-            if param.name in modified_names:
-                print(param)
-        return None
+    def report(self):
+        maybe_modified_vars = self.function_body.maybe_modified_vars
+
+        maybe_modified_vars_name = set(ident.name for ident in maybe_modified_vars)
+        param_names = set(param.name for param in self.parameter_identifiers)
+
+        modified_params = [
+            param
+            for param in self.parameter_identifiers
+            if param.name in maybe_modified_vars_name
+        ]
+        modified_vars = [var for var in maybe_modified_vars if var.name in param_names]
+
+        report = Report(
+            func=self.identifier,
+            modified_params=modified_params,
+            modified_vars=modified_vars,
+        )
+        if report.is_modified():
+            report.emit()
 
     @classmethod
     def functions_from_node(cls, node: Node) -> list["Function"]:
@@ -141,8 +157,30 @@ def get_all_descendant_with_node_type(
         yield from get_all_descendant_with_node_type(child, node_type)
 
 
+@dataclasses.dataclass
+class Report:
+    func: Identifier
+    modified_params: list[Identifier]
+    modified_vars: list[Identifier]
+
+    def is_modified(self):
+        return len(self.modified_params) and len(self.modified_vars)
+
+    def emit(self):
+        print("-" * 50)
+        print(f"Function location: {self.func}")
+
+        def str_list_with_ident(v: list[Any]) -> str:
+            return "\n".join(f"\t{str(s)}" for s in v)
+
+        print("Params location:")
+        print(str_list_with_ident(self.modified_params))
+        print("Modified vars location:")
+        print(str_list_with_ident(self.modified_vars))
+
+
 if __name__ == "__main__":
     node = get_root_node_from_source(source_code)
     functions = Function.functions_from_node(node)
     for function in functions:
-        function.find_all_ref()
+        function.report()
